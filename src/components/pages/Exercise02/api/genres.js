73ebd5d3942761bf9316d73622b5ab8060fdfa2a
Seba Sanchez <sebastian@tcit.cl>
2022-08-31 10:14:14 -0400

    chore: imported mock api json directly

--- src/components/pages/Exercise02/api/genres.js
+++ src/components/pages/Exercise02/api/genres.js
@@ -1,7 +1,12 @@
+import * as fakeApi from '../../../../mockDB.json';
+
 export const getGenres = () => (
-  fetch('http://localhost:3001/genres')
-    .then(res => res.json())
-    .catch(() => {
-      console.log('Run yarn movie-api for fake api')
-    })
+  // fetch('http://localhost:3001/genres')
+  //   .then(res => res.json())
+  //   .catch(() => {
+  //     console.log('Run yarn movie-api for fake api')
+  //   })
+  new Promise((resolve, reject) => {
+    resolve(fakeApi.genres);
+  })
 );
--- src/components/pages/Exercise02/api/movies.js
+++ src/components/pages/Exercise02/api/movies.js
@@ -1,7 +1,12 @@
+import * as fakeApi from '../../../../mockDB.json';
+
 export const getMovies = () => (
-  fetch('http://localhost:3001/movies?_limit=50')
-    .then(res => res.json())
-    .catch(() => {
-      console.log('Run yarn movie-api for fake api')
-    })
+  // fetch('http://localhost:3001/movies?_limit=50')
+  // .then(res => res.json())
+  // .catch(() => {
+  //     console.log('Run yarn movie-api for fake api')
+  // });
+  new Promise((resolve, reject) => {
+    resolve(fakeApi.movies);
+  })
 );
